9f1af3b045f4ee12902d963ef642fec4b6963a88
fivetagz-prog <fivetagz@gmail.com>
2026-06-13 22:18:17 +0800

    Replace RouterProvider with App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
+import App from "./App";
 import "./index.css";
-
-const router = createRouter({
-  routeTree
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
